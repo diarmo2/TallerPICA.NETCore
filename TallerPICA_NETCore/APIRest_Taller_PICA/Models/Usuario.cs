27b6b1c3551d6993817defde0dcbbf74045d983e
DiegoMOntano <diarmo2@hotmail.com>
2019-10-26 00:17:01 -0500

    First Commit

---
+++ TallerPICA_NETCore/APIRest_Taller_PICA/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest_Taller_PICA.Models
+{
+    public class Usuario
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string text { get; set; }
+    }
+}
---
+++ TallerPICA_NETCore/APIRest_Taller_PICA/Models/UsuarioContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRest_Taller_PICA.Models
+{
+    public class UsuarioContext :DbContext
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        public UsuarioContext(DbContextOptions<UsuarioContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
